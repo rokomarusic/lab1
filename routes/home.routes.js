c5caca891240a821fa3da4cc1257aaafc48a9065
Roko Marusic <roko.marusic@fer.hr>
2021-11-02 23:22:32 +0100

    add current user undefined check

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -2,16 +2,22 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', (req, res) => {
-    if(req.oidc.isAuthenticated()){
-        res.send("Logiran")
-    }else {
+
+    console.log(req.oidc.isAuthenticated())
+
         res.render('home', {
             locations: JSON.stringify([{lat: 40.748817, lon: -73.985428}, {
                 lat: 40.748817,
                 lon: -73.985428
-            }])
+            }]),
+            currentUser: req.oidc.isAuthenticated() ? JSON.stringify(req.oidc.user) : undefined
         })
-    }
+
 });
+
+router.post('/location', async (req, res) => {
+    const data = await req.body;
+    console.log(data)
+})
 
 module.exports = router;
